9108525c4d37d4d595005d6ea1662424eca65a98
luisJaimeB <luisyi1998@gmail.com>
2024-06-29 16:45:55 -0500

    test: crud user test.

---
+++ tests/Feature/Admin/Users/UserDestroyTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Constants\Permissions;
+use App\Constants\Roles;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use PHPUnit\Framework\Attributes\Test;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Traits\HasPermissions;
+use Tests\TestCase;
+
+class UserDestroyTest extends TestCase
+{
+    use HasPermissions;
+    use RefreshDatabase;
+
+    private const RESOURCE_NAME = 'users.destroy';
+    private string $route;
+
+    private User $customer;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->customer = User::factory()->create();
+        $this->route = route(self::RESOURCE_NAME, $this->customer);
+        
+        $adminRole = Role::create(['name' => 'Admin']);
+        $deletePermission = Permission::create(['name' => Permissions::USERS_DELETE]);
+        
+        $adminRole->givePermissionTo($deletePermission);
+
+    }
+
+    #[Test]
+    public function guest_user_cant_delete_an_user(): void
+    {
+        $response = $this->delete($this->route);
+
+        $response->assertRedirect('login');
+    }
+
+    #[Test]
+    public function unauthorized_user_can_not_delete_an_user(): void
+    {
+        /** @var \App\Models\User $user */
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->delete($this->route);
+
+        $response->assertForbidden();
+    }
+
+    #[Test]
+    public function authorized_user_can_delete_an_user(): void
+    {
+
+        /** @var \App\Models\User $user */
+        $user = User::factory()->create();
+        $user->assignRole('Admin');
+
+        $response = $this->actingAs($user)
+            ->delete($this->route);
+
+        $response->assertSessionDoesntHaveErrors()
+            ->assertRedirect(route('users.index'));
+
+        $this->assertDatabaseMissing('users', [
+            'id' => $this->customer->id,
+        ]);
+    }
+
+}
---
+++ tests/Feature/Admin/Users/UserIndexTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Constants\Permissions;
+use App\Constants\Roles;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Inertia\Testing\AssertableInertia as Assert;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Pagination\LengthAwarePaginator;
+use PHPUnit\Framework\Attributes\Test;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Traits\HasPermissions;
+use Tests\TestCase;
+
+class UserIndexTest extends TestCase
+{
+    use HasPermissions;
+    use RefreshDatabase;
+
+    private const RESOURCE_NAME = 'users.index';
+    private string $route;
+    private Role $admin;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->route = route(self::RESOURCE_NAME);
+        
+        $this->admin = Role::create(['name' => Roles::ADMIN]);
+        $readPermission = Permission::create(['name' => Permissions::USERS_INDEX]);
+        
+        $this->admin->givePermissionTo($readPermission);
+    }
+
+    #[Test]
+    public function guest_user_can_not_access_to_user_list(): void
+    {
+        $response = $this->get($this->route);
+
+        $response->assertRedirect('login');
+    }
+
+    #[Test]
+    public function unauthorized_user_can_not_access_to_user_list(): void
+    {
+        /** @var \App\Models\User $user */
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->get($this->route);
+
+        $response->assertForbidden();
+    }
+
+    #[Test]
+    public function authorized_user_can_access_to_user_list(): void
+    {
+
+        /** @var \App\Models\User $user */
+        $user = User::factory()->create();
+        $user->assignRole($this->admin);
+
+        /** @var \App\Models\User $customer */
+        $customer = User::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->get($this->route);
+
+        $response->assertOk()
+            ->assertInertia(fn (Assert $page) => $page
+                ->component('Admin/Users/Index')
+                ->where('users.current_page', 1) 
+                ->where('users.total', 2) 
+                ->where('users.per_page', 3) 
+                ->where('users.first_page_url', 'http://localhost/admin/users?page=1') 
+                ->where('users.last_page_url', 'http://localhost/admin/users?page=1') 
+                ->where('users.prev_page_url', null) 
+                ->where('users.next_page_url', null)
+                ->has('users.links', 3)
+            )
+            ->assertSee($customer->name);
+            /* ->assertSee(route('users.create'))
+            ->assertSee(route('users.edit', $customer->id))
+            ->assertSee(route('users.destroy', $customer->id)); */
+    }
+}
