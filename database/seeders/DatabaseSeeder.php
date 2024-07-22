212579c95dfc043d717bba5f9503636a15bb81d7
luisJaimeB <luisyi1998@gmail.com>
2024-07-22 11:08:25 -0500

    feat: The currencies seed is added.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-/* 
+/*
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             CategorySeeder::class,
             TypeSiteSeeder::class,
+            CurrencySeeder::class,
         ]);
     }
 }
