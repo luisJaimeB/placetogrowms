4fb315f7cbe1c383f03586f1c4e4ad2d5afac3cf
luisJaimeB <luisyi1998@gmail.com>
2024-07-29 15:45:27 -0500

    refactor: Strict typing is performed on the tables.

--- database/migrations/2024_06_29_221642_create_microsites_table.php
+++ database/migrations/2024_06_29_221642_create_microsites_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('microsites', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
 
             $table->foreignId('type_site_id');
             $table->foreign('type_site_id')
