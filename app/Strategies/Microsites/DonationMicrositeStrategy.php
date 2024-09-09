92bfffabf50dfbdf23069c720d95e9e846cd06cf
luisJaimeB <luisyi1998@gmail.com>
2024-09-08 19:58:37 -0500

    feat: New strategy for site types and forms.

--- app/Strategies/Microsites/DonationMicrositeStrategy.php
+++ app/Strategies/Microsites/DonationMicrositeStrategy.php
@@ -6,7 +6,6 @@ use App\Models\Microsite;
 
 class DonationMicrositeStrategy implements MicrositeStrategy
 {
-
     public function renderComponent(Microsite $microsite): string
     {
         return 'Payments/CreatePayment';
